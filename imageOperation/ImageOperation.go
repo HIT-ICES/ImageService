9e424d9b93fe83429fa52a8be5c3624069a446be
MicroYui <1048354041@qq.com>
2023-11-26 19:16:51 +0800

    feat: modify image list

--- imageOperation/ImageOperation.go
+++ imageOperation/ImageOperation.go
@@ -8,9 +8,16 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"strings"
 )
 
-func ListImages() []images.Image {
+type modifiedImage struct {
+	ImageName    string `json:"imageName"`
+	ImageVersion string `json:"imageVersion"`
+	ImageSize    int64  `json:"imageSize"`
+}
+
+func ListImages() []modifiedImage {
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
 		log.Println(err)
@@ -20,11 +27,37 @@ func ListImages() []images.Image {
 	ctx := namespaces.WithNamespace(context.Background(), "k8s.io")
 
 	imageList, err := client.ImageService().List(ctx)
+
+	var modifiedImageList []modifiedImage
+	for _, image := range imageList {
+		oldName := image.Name
+		var name string
+		var version string
+		if strings.Contains(oldName, "@") {
+			name = oldName[:strings.LastIndex(oldName, "@")]
+			version = ""
+		} else {
+			if strings.Contains(oldName, "sha256") {
+				name = oldName
+				version = ""
+			} else {
+				lastIndex := strings.LastIndex(oldName, ":")
+				if lastIndex != -1 {
+					name = oldName[:lastIndex]
+					version = oldName[lastIndex+1:]
+				}
+			}
+		}
+		size := image.Target.Size
+		modifyingImage := modifiedImage{name, version, size}
+		modifiedImageList = append(modifiedImageList, modifyingImage)
+	}
+
 	if err != nil {
 		log.Println(err)
 	}
 
-	return imageList
+	return modifiedImageList
 }
 
 func PullImage(imageName string) string {
